4c9f5ddf11b20c1668de9fef0080e05f8dfa73e3
yuliaveres <juliaveresklia@gmail.com>
2017-11-16 15:41:14 +0100

    Use 32 length id for groups

--- Toshi/Controllers/Chat/ChatInteractor.swift
+++ Toshi/Controllers/Chat/ChatInteractor.swift
@@ -323,7 +323,7 @@ final class ChatInteractor: NSObject {
 
     public static func createGroup(with recipientsIds: NSMutableArray = ["0xfed3c4db1dc5878fd19c401754dd1e887da8af03", "0x60c7c1935f787a6474b84f3a997263080bb20b1c", "0xd79ade23a00ebbf1485938f375f1310ca5b57b6a"], name: String, avatar: UIImage? = UIImage(named: "white")) {
 
-        let groupId = Randomness.generateRandomBytes(16)
+        let groupId = Randomness.generateRandomBytes(32)
         let image = UIImage(named: "white")
         guard let groupModel = TSGroupModel(title: name, memberIds: recipientsIds, image: image, groupId: groupId) else { return }
 
